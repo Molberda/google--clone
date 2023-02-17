c5610c26b0535774b9a8c4807462d05c8ff12d90
David Moller <davidmollera@hotmail.com>
2023-02-16 19:20:35 -0500

    adding material ui buttons to the search bar component

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import SearchIcon from '@mui/icons-material/Search'
-import MicIcon from '@mui/icons-material/Mic'
+import "./SearchBar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import MicIcon from "@mui/icons-material/Mic";
+import { Button } from "@mui/material";
 
 const SearchBar = () => {
   return (
-    <div className="search__input">
-      <MicIcon className="search__input--micIcon" />
-      <input />
-      <SearchIcon className="search__input--searchIcon" />
+    <div className="search">
+      <div className="search__input">
+        <MicIcon className="search__input--micIcon" />
+        <input placeholder="What are you looking for?" />
+        <SearchIcon className="search__input--searchIcon" />
+      </div>
+
+      <div className="search__btns">
+        <Button variant="outlined">Google Search</Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
+      </div>
     </div>
   );
 };
