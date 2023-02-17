51d3cc9edc2bcdde03eb18cc92e26dae92573e9d
David Moller <davidmollera@hotmail.com>
2023-02-16 19:45:41 -0500

    using the use state hook for the search function

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
 import SearchIcon from "@mui/icons-material/Search";
 import MicIcon from "@mui/icons-material/Mic";
 import { Button } from "@mui/material";
 
 const SearchBar = () => {
-  return (
+
+    const [input, setInput] = useState('');
+
+    function search(e) {
+        e.preventDefault()
+    } 
+
+    return (
     <div className="search">
       <div className="search__input">
         <MicIcon className="search__input--micIcon" />
@@ -14,7 +21,7 @@ const SearchBar = () => {
       </div>
 
       <div className="search__btns">
-        <Button variant="outlined">Google Search</Button>
+        <Button onClick={search} variant="outlined">Google Search</Button>
         <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
     </div>
