a5676eb635259530eaa1029a6d392f15c6f04265
David Moller <davidmollera@hotmail.com>
2023-02-17 12:05:59 -0500

    added some tweaks to css for aesthetics

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -3,28 +3,40 @@ import "./SearchBar.css";
 import SearchIcon from "@mui/icons-material/Search";
 import MicIcon from "@mui/icons-material/Mic";
 import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+  const [input, setInput] = useState("");
 
-    const [input, setInput] = useState('');
+  const navigate = useNavigate
 
-    function search(e) {
-        e.preventDefault()
-    } 
+  function search(e) {
+    e.preventDefault();
+    console.log(input)
+    // Do something with that input... come back and fix it
 
-    return (
-    <div className="search">
+    // 
+  }
+
+  return (
+    <form className="search">
       <div className="search__input">
         <MicIcon className="search__input--micIcon" />
-        <input placeholder="What are you looking for?" />
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="What are you looking for?"
+        />
         <SearchIcon className="search__input--searchIcon" />
       </div>
 
       <div className="search__btns">
-        <Button onClick={search} variant="outlined">Google Search</Button>
+        <Button type="submit" onClick={search} variant="outlined">
+          Google Search
+        </Button>
         <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
-    </div>
+    </form>
   );
 };
 
