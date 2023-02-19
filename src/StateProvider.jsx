a4b47d7ca96c45471683e51ca9b23d1daf5a82de
David Moller <davidmollera@hotmail.com>
2023-02-18 22:25:43 -0500

    passed props to the stateprovider in index.js and created a reducer file

--- src/StateProvider.jsx
+++ src/StateProvider.jsx
@@ -11,4 +11,5 @@ export const StateProvider = ({ reducer, initialState, children
         </StateContext.Provider>
 );
 
+// Hook which allows us to pull information from the data layer 
 export const useStateValue = () => useContext(StateContext)
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import './StateProvider.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <StateProvider> */}
+    <StateProvider initalState={initialState} reducer={reducer}>
       <App />
-    {/* </StateProvider> */}
+    </StateProvider>
   </React.StrictMode>
 );
 
