b00cdba4a9ccef91d061410dec3dc2fe67b9f202
David Moller <davidmollera@hotmail.com>
2023-02-18 22:45:46 -0500

    added the correct imports to the index.js file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import './StateProvider.jsx'
+import {StateProvider} from './StateProvider.jsx'
+import reducer, {initialState} from './reducer.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
