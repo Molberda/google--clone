103c7bfe478c59d64af3adf4b42cc2aac7c6ffb0
David Moller <davidmollera@hotmail.com>
2023-02-19 21:51:11 -0500

    creating the custom useGoogleSearch hook

--- src/useGoogleSearch.jsx
+++ src/useGoogleSearch.jsx
@@ -1,12 +1,16 @@
 // Search engine ID 917aef3cd46d24488
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import API_KEY from './Keys';
 
-const useGoogleSearch = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const CONTEXT_KEY = '917aef3cd46d24488'
+
+const useGoogleSearch = (term) => {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        
+    }, [term]);
+
 }
 
 export default useGoogleSearch;
