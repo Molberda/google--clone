bd33b0760eb0e607f8a6b3fd10238c672ed04882
David Moller <davidmollera@hotmail.com>
2023-02-19 23:06:14 -0500

    added the api link for the googlesearch file

--- src/useGoogleSearch.jsx
+++ src/useGoogleSearch.jsx
@@ -8,8 +8,14 @@ const useGoogleSearch = (term) => {
     const [data, setData] = useState(null)
 
     useEffect(() => {
+      const fetchData = async() => {
+        fetch `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      }
         
+
     }, [term]);
+
+    return { data }
 
 }
 
