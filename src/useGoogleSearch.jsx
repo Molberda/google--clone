fc27cff4f8e28916bd213567f7e8c199e60c1632
David Moller <davidmollera@hotmail.com>
2023-02-19 21:46:10 -0500

    created a usegooglesearch file

---
+++ src/useGoogleSearch.jsx
@@ -0,0 +1,12 @@
+// Search engine ID 917aef3cd46d24488
+import React from 'react';
+
+const useGoogleSearch = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default useGoogleSearch;
