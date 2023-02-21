f415a52086f1c12db5ee2232c229e26ef199ee22
David Moller <davidmollera@hotmail.com>
2023-02-20 23:55:27 -0500

    added row and container divs to the search page

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,38 +1,41 @@
-import React from 'react';
-import './Search.css'
-import { Link } from 'react-router-dom';
-import SearchBar from '../Components/SearchBar';
-import { useStateValue } from '../StateProvider';
-import useGoogleSearch from '../useGoogleSearch';
-import response from '../response';
-
+import React from "react";
+import "./Search.css";
+import { Link } from "react-router-dom";
+import SearchBar from "../Components/SearchBar";
+import { useStateValue } from "../StateProvider";
+import useGoogleSearch from "../useGoogleSearch";
+import response from "../response";
 
 const Search = () => {
-    const [{ term }, dispatch] = useStateValue()
-    // const { data } = useGoogleSearch(term)
+  const [{ term }, dispatch] = useStateValue();
+  // const { data } = useGoogleSearch(term)
 
-    const data = response
+  const data = response;
 
-    console.log(data)
+  console.log(data);
 
-    return (
-        <div className='search'>
-            <div className="search__header">
-            <Link to='/'>
-                <figure className="search__logo--wrapper">
-                    <img src="https://cdn2.downdetector.com/static/uploads/logo/Google-new_19.png" alt="" className="search__logo" />
-                </figure>
+  return (
+    <div className="search">
+      <div className="container search__container">
+        <div className="row search__row">
+          <div className="search__header">
+            <Link to="/">
+              <figure className="search__logo--wrapper">
+                <img
+                  src="https://cdn2.downdetector.com/static/uploads/logo/Google-new_19.png"
+                  alt=""
+                  className="search__logo"
+                />
+              </figure>
             </Link>
-                
-            </div>
+          </div>
 
-            <div className="search__results">
-
-
-            </div>
-            <SearchBar hideButtons/>                                                                                                                                                                                 
+          <div className="search__results"></div>
+          <SearchBar hideButtons />
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Search;
